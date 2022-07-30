16b65d9cd580c629501e3f1d5773d553fe58d77f
Pedro Santana Neves <pneves@bladeinsight.com>
2022-07-30 14:03:35 +0100

    feat: add cors middleware

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "mostra-os-dentes-paulinho.netlify.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.get("/", (req, res) => {
   res.send("From Vercel hosting");
 });
